1257a82ca9024873a4082895aadf1edaf1a393c1
Thales Reis <thales.areis@gmail.com>
2019-11-27 23:58:10 -0300

    new db host

---
+++ FrontSystemWine/DTOS/AtributosPesquisaVinho.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontSystemWine.DTOS
+{
+    public class AtributosPesquisaVinho
+    {
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public int IdTipoVinho { get; set; }
+
+        public int IdTipoUva { get; set; }
+
+        public int IdPais { get; set; }
+
+        public int IdRegiao { get; set; }
+    }
+}
